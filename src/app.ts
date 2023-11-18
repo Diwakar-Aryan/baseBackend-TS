9e00512fbd6c2726271054e8af532ce95557df27
Diwakar Singh <aryan90067@gmail.com>
2023-11-19 05:09:26 +0530

    Added error middleware and Response wrapper

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,45 @@
-import express from 'express'
-import bodyParser from 'body-parser';
-import { Routes } from './interfaces/routes.interface';
-import configClass from './configs';
-
-
+import express from "express";
+import bodyParser from "body-parser";
+import { Routes } from "./interfaces/routes.interface";
+import configClass from "./configs";
+import { errorMiddleware } from "./middlewares/error.middleware";
 class App {
-    private app: express.Application
-    private config = configClass.initialize()
-    constructor(routes: Routes[]){
-        
-        this.app = express()
-        this.app.use(bodyParser.json())
-        this.initializeRoutes(routes,'/api/')
+  private app: express.Application;
+  private config = configClass.initialize();
+  constructor(routes: Routes[]) {
+    this.app = express();
+    this.initializeMiddlewares()
+    this.app.use(bodyParser.json());
+    this.initializeRoutes(routes, "/api/");
+    this.initializeErrorHandling()
+  }
 
-    }
-    
-    public async listen(){
-        this.app.listen(this.config.ServerInfo.PORT,()=>{
-            console.log(`App listening on port ${this.config.ServerInfo.PORT}`)
-        })
-    }
+  public async listen() {
+    this.app.listen(this.config.ServerInfo.PORT, () => {
+      console.log(`App listening on port ${this.config.ServerInfo.PORT}`);
+    });
+  }
 
-    public get Server() {
-        return this.app;
-    }
+  public get Server() {
+    return this.app;
+  }
 
-    private initializeRoutes(routes: Routes[],basePath:string){
-        routes.forEach((route)=>{
-            this.app.use(basePath,route.router)
-        })
-    }
+  private initializeMiddlewares() {
+ 
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
 
+  }
+
+  private initializeRoutes(routes: Routes[], basePath: string) {
+    routes.forEach((route) => {
+      this.app.use(basePath, route.router);
+    });
+  }
+
+  private initializeErrorHandling() {
+    this.app.use(errorMiddleware);
+  }
 }
 
 export default App;
